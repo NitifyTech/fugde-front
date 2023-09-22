eef9e4c6f3a329898b9eeb64c49a458274d30df5
PseudoHUEry <harry-kevin@hotmail.com>
2023-09-21 21:13:59 -0300

    chore(_app.tsx): add type annotation to the Home component props to improve code readability and maintainability

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function Home({ Component, pageProps }){
+export default function Home({ Component, pageProps }:any){
     return <Component {...pageProps}/>
 }
