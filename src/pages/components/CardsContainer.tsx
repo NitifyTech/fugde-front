f3fc4f472b456def51faf87538a6f4bb2ed313d7
PseudoHUEry <harry-kevin@hotmail.com>
2023-09-22 10:52:30 -0300

    feat(CallToAction.tsx): add CallToAction component to display a call to action message and short description feat(CardsContainer.tsx): add CardsContainer component to display a list of cards with title, description, and published date feat(index.tsx): implement CallToAction and CardsContainer components in the Home page to display dynamic data and improve user experience

---
+++ src/pages/components/CardsContainer.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export interface IMountCard {
+    id: string;
+    registryType: string;
+    redirectLink?: string;
+    cardTitle: string;
+    cardDescription: string,
+    publishedDate: string;
+    imageLink?: string;
+}
+
+export interface ICardsContainer {
+    title: string;
+    shortDescription: string;
+    cardsList: IMountCard[]
+}
+
+function mountCard({ cardTitle, id, publishedDate, registryType, redirectLink, imageLink, cardDescription }: IMountCard, key: number) {
+    const link = redirectLink ?? `${registryType}/${id}`
+    return (
+        <div className="shadow-md hover:shadow-lg" key={key}>
+            {imageLink && <a href={link}>
+                <img src={imageLink} className="object-cover w-full h-56 mb-5 bg-center rounded" loading="lazy" />
+            </a>}
+            <h2 className="mb-2 text-lg font-semibold text-gray-900">
+                <a href={link} className="text-gray-900 hover:text-purple-700">{cardTitle}</a>
+            </h2>
+            {cardDescription && <p className="mb-3 text-sm font-normal text-gray-500">
+                {cardDescription}
+            </p>}
+            <p className="mb-3 text-sm font-normal text-gray-500">
+                {new Date(publishedDate).toLocaleDateString('pt-BR')}
+            </p>
+        </div>
+    )
+}
+
+export default function CardsContainer({ shortDescription, title, cardsList }: ICardsContainer) {
+    return (
+        <section className="px-4 py-8 mx-auto max-w-7xl">
+            {title && <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">{title}</h2>}
+            {shortDescription && <p className="mb-20 text-lg text-gray-500">{shortDescription}</p>}
+            <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 ">
+                {cardsList && cardsList.map((card, idx) => mountCard(card, idx))}
+            </div>
+        </section>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,54 @@
 import React from "react"
 
+import { useState } from 'react'
+import CallAction, { ICallToAction } from '@/pages/components/CallToAction'
+import CardsContainer, { ICardsContainer } from '@/pages/components/CardsContainer'
+
 export default function Home() {
+    const [callActionData, setCallActionData] = useState<ICallToAction>({
+        callActionMessage: "Aguarde enquanto processamos vossa solicitação.",
+        shortMessage: "Isso levará alguns segundos."
+    })
+
+    const [cardsContainerData, setCardsContainerData] = useState<ICardsContainer>({
+        shortDescription: "a",
+        title: "a",
+        cardsList: [{
+            cardTitle: "Tilápias",
+            cardDescription: "aaaaaaaa",
+            id: "1",
+            publishedDate: new Date().toISOString(),
+            registryType: "species",
+            imageLink: "",
+        }, {
+            cardTitle: "Tilápias",
+            cardDescription: "aaaaaaaa",
+            id: "1",
+            publishedDate: new Date().toISOString(),
+            registryType: "species",
+            imageLink: "",
+        }, {
+            cardTitle: "Tilápias",
+            cardDescription: "aaaaaaaa",
+            id: "1",
+            publishedDate: new Date().toISOString(),
+            registryType: "species",
+            imageLink: "",
+        }]
+    })
+
     return (
         <>
-            <section className="px-4 py-20 bg-gradient-to-r from-white to-purple-100">
-                <div className="w-full h-full grid items-center grid-cols-1 gap-4 mx-auto max-w-7xl lg:grid-cols-2 xl:grid-cols-3">
-                    <div>
-                        <p className="mb-1 text-base font-medium md:text-xl text-primary">Crie suas estatisticas</p>
-                        <h2 className="mb-6 text-3xl font-bold md:leading-tight md:text-4xl md:mb-4">Inclua seus dados de caracterização</h2>
-                    </div>
-                </div>
-            </section>
-            <section className="px-4 py-8 mx-auto max-w-7xl">
-                <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Skcript Blog</h2>
-                <p className="mb-20 text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-                    <div>
-                        <a href="#">
-                            <img src="/brand/og.png" className="object-cover w-full h-56 mb-5 bg-center rounded" alt="Kutty" loading="lazy" />
-                        </a>
-                        <h2 className="mb-2 text-lg font-semibold text-gray-900">
-                            <a href="#" className="text-gray-900 hover:text-purple-700">Lorem ipsum dolor sit amet consectetur adipisicing elit.</a>
-                        </h2>
-                        <p className="mb-3 text-sm font-normal text-gray-500">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ipsa quibusdam, reprehenderit odit magni voluptates voluptatum error quasi nihil alias quam, dolor beatae ipsam in eveniet soluta ullam, unde ratione!
-                        </p>
-                        <p className="mb-3 text-sm font-normal text-gray-500">
-                            <a href="#" className="font-medium text-gray-900 hover:text-purple-700">Praveen Juge</a>
-                            • April 16, 2020
-                        </p>
-                    </div>
-                </div>
-            </section>
+            <CallAction
+                {...callActionData}
+            />
+            {
+                cardsContainerData.cardsList.length > 0
+                &&
+                <CardsContainer
+                    {...cardsContainerData}
+                />
+            }
         </>
     )
 }
